2b023036b84ff2e4386065474d5a9b296e9f7a42
Maddy <madisonschaaf48@gmail.com>
2022-04-19 10:03:58 -0600

    added routes for post

--- models/Post.js
+++ models/Post.js
@@ -14,10 +14,17 @@ Post.init(
         title: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        // //post_url?13.3.4
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              isURL: true
+            }
         // },
-        // //post_url?
         //     user_id: {
-        //         type: DataTypes.STRING,
+        //         type: DataTypes.INTEGER,
         //         references: {
         //             model: 'user',
         //             key: 'id'
--- models/index.js
+++ models/index.js
@@ -3,5 +3,15 @@ const User = require('./User')
 const Comment = require('./Comment')
 const Post = require('./Post')
 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+
+
 
 module.exports = { User, Comment, Post}
